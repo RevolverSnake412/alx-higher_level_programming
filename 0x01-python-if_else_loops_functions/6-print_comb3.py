9f79d06ccaa4cb1a443fb5d33c40a17cebc7cb74
RevolverSnake412 <oussag@hotmail.fr>
2023-09-06 00:39:02 +0100

    update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i + 1, 10):
-        print(f"{i}{j}", end="")
+        print("{}".format(i) + "{}".format(j), end="")
         if i == 8:
             print()
         else:
