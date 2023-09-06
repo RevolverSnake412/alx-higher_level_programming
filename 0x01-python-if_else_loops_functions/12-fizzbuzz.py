cddb1d22e5212844cbb70f5d59f7ef1d4aaeb2b4
RevolverSnake412 <oussag@hotmail.fr>
2023-09-06 01:24:29 +0100

    update

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(1, 101):
-    if i % 3 == 0 and i % 5 ==0:
+    if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz", end=" ")
     elif i % 3 == 0:
         print("Fizz", end=" ")
@@ -9,4 +9,4 @@ for i in range(1, 101):
     else:
         print(i, end=" ")
     if i == 100:
-        print("$")
+        print("")
