beb1632c5d060ee9eff8ebb82d3a77cffe8184ff
RevolverSnake412 <oussag@hotmail.fr>
2023-09-07 16:06:28 +0100

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,13 @@ if __name__ == "__main__":
     import sys
 
 argv_num = len(sys.argv)
-print("{} arguments".format(argv_num - 1), end="")
+print("{} argument".format(argv_num - 1), end="")
 if argv_num == 1:
-    print(".")
+    print("s.")
 else:
-    print(":")
+    if argv_num == 2:
+        print(":")
+    else:
+        print("s:")
     for i in range(1, argv_num):
         print("{}: {}".format(i, sys.argv[i]))
