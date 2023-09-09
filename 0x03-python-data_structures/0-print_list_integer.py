f217ea76fc9a0ac6abd5cc932e2703461d7318f2
RevolverSnake412 <oussag@hotmail.fr>
2023-09-09 16:27:53 +0100

    update

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for element in my_list:
-        if element is str:
-            print(str.format(element))
-        else:
-            print(element)
+        print("{}".format(element))
