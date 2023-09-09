0a8360d1506f652ac38b77ea160aa821c963304b
RevolverSnake412 <oussag@hotmail.fr>
2023-09-09 16:41:15 +0100

    update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     size = len(my_list)
-    if idx >= size or my_list[idx] < 0:
+    if idx >= size or idx < 0:
         return None
     else:
         return my_list[idx]
