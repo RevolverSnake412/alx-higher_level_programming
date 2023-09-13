a470f92974832e0bd992c5b874c9e8d02ae28bb8
RevolverSnake412 <oussag@hotmail.fr>
2023-09-13 20:28:27 +0100

    update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str):
-        return 0
-
     dict_roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     flag = 0
     decimal = 0
+
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
 
     for i in range(len(roman_string)):
         cur_num = dict_roman.get(roman_string[i])
@@ -23,19 +23,3 @@ def roman_to_int(roman_string):
             if flag == 0:
                 decimal += cur_num
     return decimal
-
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
