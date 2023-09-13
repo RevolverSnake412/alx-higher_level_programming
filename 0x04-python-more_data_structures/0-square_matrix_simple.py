5967f12cbe04c1a3b5114ddf51659ee74d9ce633
RevolverSnake412 <oussag@hotmail.fr>
2023-09-13 17:05:18 +0100

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[row[i] for row in matrix] for i in range(len(matrix))]
+    new_matrix = [[row[i] ** 2 for i in range(len(matrix))] for row in matrix]
     return new_matrix
