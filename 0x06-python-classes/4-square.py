166c8edcc31f196089eb2f0ec0d2218f1101f369
RevolverSnake412 <oussag@hotmail.fr>
2023-09-26 22:37:08 +0100

    update

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -17,11 +17,11 @@ class Square:
     @size.setter
     def size(self, value):
         """setter"""
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """returns current square area"""
