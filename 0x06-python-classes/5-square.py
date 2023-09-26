3b2c5537b5c3bea22beefa5931aeae2ac3c60d6d
RevolverSnake412 <oussag@hotmail.fr>
2023-09-26 22:44:26 +0100

    ugly whitespace

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -32,6 +32,6 @@ class Square:
             print("")
         else:
             for i in range(self.__size):
-                for j in range (self.__size):
+                for j in range(self.__size):
                     print("#", end="")
                 print("")
