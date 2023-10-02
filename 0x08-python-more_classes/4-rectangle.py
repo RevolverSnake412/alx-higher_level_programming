2d1108ad4d47f4dafad4cd502dbbf1e4ad5922e3
RevolverSnake412 <oussag@hotmail.fr>
2023-10-02 23:34:42 +0100

    pycodestyle

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -47,7 +47,7 @@ class Rectangle:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
-    def __str__(self): 
+    def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ("")
 
