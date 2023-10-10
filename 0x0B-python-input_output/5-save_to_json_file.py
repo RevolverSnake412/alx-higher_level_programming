7bcd9dc67b4f5a22f7df0c1cc8392751bac6dedb
RevolverSnake412 <oussag@hotmail.fr>
2023-10-10 22:54:10 +0100

    update

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,6 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """saves to a json file"""
-    text = json.dumps(my_obj)
     with open(filename, "w", endcoding="UTF8") as fhand:
         json.dump(my_obj, fhand)
