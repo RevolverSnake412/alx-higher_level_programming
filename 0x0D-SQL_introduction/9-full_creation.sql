845140641b5f11e1fa993dbc579e46c844e16ca0
RevolverSnake412 <oussag@hotmail.fr>
2023-11-14 19:51:01 +0000

    TASK 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- Creates a second_table in the DB, and adds multiples rows.
+-- [Usage]: cat <file-name> | mysql -hlocalhost -uroot -p <db>
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
