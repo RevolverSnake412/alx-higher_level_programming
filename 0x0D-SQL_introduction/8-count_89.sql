e93a614dab192da60fa8e91630538fdff314a895
RevolverSnake412 <oussag@hotmail.fr>
2023-11-14 19:48:40 +0000

    TASK 8

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- Displays the number of records with id = 89 in first_table.
+-- [Usage]: cat <file-name> | mysql -hlocalhost -uroot -p <db> | tail -1
+SELECT COUNT(*) FROM first_table WHERE id = 89;
