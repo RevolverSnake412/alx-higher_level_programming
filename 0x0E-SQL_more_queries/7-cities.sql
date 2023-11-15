19875eee938b495ca249b9528b6212ad5b03a954
RevolverSnake412 <oussag@hotmail.fr>
2023-11-15 17:40:27 +0000

    TASK 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- Creates database and its table.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INTEGER UNIQUE AUTO_INCREMENT NOT NULL,
+state_id INTEGER NOT NULL,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY(state_id) REFERENCES states(id));
