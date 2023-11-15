edc686fccea450bddec5b4d00a7bff0a7371241e
RevolverSnake412 <oussag@hotmail.fr>
2023-11-15 17:34:05 +0000

    TASK 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- Creates the database and its table.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INTEGER PRIMARY KEY, name VARCHAR(256) NOT NULL);
