acfc0a726dede076f185f685ef7c2787e0c8e136
RevolverSnake412 <oussag@hotmail.fr>
2023-11-15 17:25:59 +0000

    typo fix

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- Creates table force_name.
-CREATE TABLE IF NOT EXISTS force_name (id INTEGER, namee VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (id INTEGER, name VARCHAR(256) NOT NULL);
