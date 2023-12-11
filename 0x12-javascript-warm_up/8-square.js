694fa2776f41143d1c360fb024c3d3901b5999f0
RevolverSnake412 <oussag@hotmail.fr>
2023-12-11 15:43:39 +0000

    TASK 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Missing size');
+} else {
+  const x = process.argv[2];
+  for (let i = 0; i < x; i++) {
+    console.log('X'.repeat(x));
+  }
+}
