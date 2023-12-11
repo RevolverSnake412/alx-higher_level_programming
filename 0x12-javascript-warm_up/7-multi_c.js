c0ea87d780c4d454b11314dd6d7e4eb9ffa66439
RevolverSnake412 <oussag@hotmail.fr>
2023-12-11 16:24:34 +0000

    update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+if (process.argv.length <= 2 || isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  const x = process.argv[2];
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
