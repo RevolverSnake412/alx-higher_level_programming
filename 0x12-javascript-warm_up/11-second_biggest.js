3b9a3d9cabd924ae151d81172d2df3a174bc2720
RevolverSnake412 <oussag@hotmail.fr>
2023-12-11 16:15:34 +0000

    TASK 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+if (process.argv.length <= 3) {
+  console.log('0');
+} else {
+  const numArray = process.argv.slice(2).map(Number);
+  numArray.sort(function (a, b) {
+    return a - b;
+  });
+  console.log(numArray[numArray.length - 2]);
+}
