9df807d4f5b2bab214b8a481302b78b2b81d956b
RevolverSnake412 <oussag@hotmail.fr>
2024-01-06 17:05:38 +0000

    TASK 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-''''''
+'''sends a request to the url and outputs expected error'''
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
