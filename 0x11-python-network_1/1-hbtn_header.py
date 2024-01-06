d8804255fde5239f6e5b8beffc76ba12eb9b64d9
RevolverSnake412 <oussag@hotmail.fr>
2024-01-06 16:43:16 +0000

    TASK 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-''''''
+'''sends a request to the URL and outputs X-Request-ID'''
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = urllib.request.Request(url)
+
+    with urllib.request.urlopen(req) as response:
+        print(dict(response.headers).get("X-Request-Id"))
