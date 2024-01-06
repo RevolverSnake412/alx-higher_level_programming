f722bd06ae76db6caffe351fe7d00f1fbad5dd7a
RevolverSnake412 <oussag@hotmail.fr>
2024-01-06 16:36:16 +0000

    TASK 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-''''''
+'''Fetches the URL below'''
+import urllib.request
+
+req = urllib.request.urlopen('https://alx-intranet.hbtn.io/status')
+answer = req.read()
+print("Body response:")
+print("\t- type: {}".format(type(answer)))
+print("\t- content: {}".format(answer))
+print("\t- utf8 content: {}".format(answer.decode('utf-8')))
