b344417dd651b59f2580209f3539d566351a4810
RevolverSnake412 <oussag@hotmail.fr>
2024-01-06 17:03:09 +0000

    TASK 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-''''''
+'''sends a POST req, takes email as parameter'''
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import sys
+
+    url = sys.argv[1]
+    email = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(email).encode("ascii")
+    req = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
