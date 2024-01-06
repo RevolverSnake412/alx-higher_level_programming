4cce1870b69cdc1b4f1e465ba9748e191f2f3550
RevolverSnake412 <oussag@hotmail.fr>
2024-01-06 17:52:41 +0000

    update

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,7 +6,9 @@ if __name__ == "__main__":
     import requests
 
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
+    request = requests.get(url)
 
-    request = requests.post(url, data=value)
-    print(request.text)
+    if request.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(request.text)
