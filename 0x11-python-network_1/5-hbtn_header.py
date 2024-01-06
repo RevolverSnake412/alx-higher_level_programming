c89ebe1ab19df6e05450977918e9ee76068c1959
RevolverSnake412 <oussag@hotmail.fr>
2024-01-06 17:12:23 +0000

    TASK 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-''''''
+'''sends a request and displays the values of x-Request-ID'''
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+
+    req = requests.get(url)
+    print(req.headers.get("X-Request-Id"))
