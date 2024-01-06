eaf6d326b28c4d07802b0e4d5de2230a124167fe
RevolverSnake412 <oussag@hotmail.fr>
2024-01-06 17:09:26 +0000

    update typo

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@
 if __name__ == "__main__":
     import requests
 
-    req = requests.get("https://intranet.hbtn.io/status")
+    req = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(req.text)))
     print("\t- content: {}".format(req.text))
