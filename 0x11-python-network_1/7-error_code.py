b27429c6df5f46ecb96fb62f741c7519b3484f03
RevolverSnake412 <oussag@hotmail.fr>
2024-01-06 17:53:38 +0000

    typo

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     request = requests.get(url)
 
     if request.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
+        print("Error code: {}".format(request.status_code))
     else:
         print(request.text)
