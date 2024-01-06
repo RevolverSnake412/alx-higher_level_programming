0c33f76cacbc26208a09a433ec0e7b36bf56faa4
RevolverSnake412 <oussag@hotmail.fr>
2024-01-06 17:41:55 +0000

    TASK 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-''''''
+'''sends a POST request to the passed URL with email par'''
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
