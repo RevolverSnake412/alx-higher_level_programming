49e6504945388fd07de395d94b8f81db8db959f9
RevolverSnake412 <oussag@hotmail.fr>
2024-01-06 17:49:56 +0000

    TASK 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-''''''
+'''sends a request to the URL and displays the body of the response'''
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    request = requests.post(url, data=value)
+    print(request.text)
