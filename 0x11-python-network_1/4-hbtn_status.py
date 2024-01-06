397e411e26599f89688e2cc2c9a9c96a22719152
RevolverSnake412 <oussag@hotmail.fr>
2024-01-06 17:07:36 +0000

    TASK 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-''''''
+"""fetches https://intranet.hbtn.io/status."""
+
+
+if __name__ == "__main__":
+    import requests
+
+    req = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
