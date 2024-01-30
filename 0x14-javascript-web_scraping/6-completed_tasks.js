972b6e1cad835c7430d6e537e76f60edd089d16f
RevolverSnake412 <oussag@hotmail.fr>
2024-01-30 18:47:55 +0000

    refactor

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
 const array = {};
-let comp = 1;
 let tempId = 1;
 
 request(process.argv[2], function (error, response, body) {
   if (!error) {
     const parsedJSON = JSON.parse(body);
+    array[parsedJSON[0].userId] = 0;
     for (const i in parsedJSON) {
       if (tempId !== parsedJSON[i].userId) {
         tempId = parsedJSON[i].userId;
-        comp = 1;
+        array[parsedJSON[i].userId] = 0;
       }
       if (parsedJSON[i].completed === true) {
-        array[tempId] = comp++;
+        array[parsedJSON[i].userId]++;
       }
     }
     console.log(array);
