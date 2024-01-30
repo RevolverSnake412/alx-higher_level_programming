1d6d0fa643df65b35e155e6270d15e694453c5d0
RevolverSnake412 <oussag@hotmail.fr>
2024-01-30 17:27:24 +0000

    TASK 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  const parsedJSON = JSON.parse(body);
+  console.log(parsedJSON.title);
+});
